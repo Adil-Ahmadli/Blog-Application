06000a2378d4c41deacab589bee942ea92da1db5
Adil Ahmadli <adil.ahmadli@proton.me>
2023-09-17 18:31:17 +0300

    Moved Mongo credentials to .env file

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose"
 const app = express()
 
 mongoose
-.connect("mongodb+srv://admin:admin@mern.mw3gtmd.mongodb.net/?retryWrites=true&w=majority")
+.connect(process.env.MONGO_URL)
 .then(()=>app.listen(5000))
 .then(()=>console.log("Everything is fine!"))
 .catch((err)=>console.log(err))
